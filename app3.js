28034f20982680b05b9e4539da4574f3ace8e76a
Karif Williams <kariftw@hotmail.com>
2021-10-12 15:19:20 -0400

    update

---
+++ app3.js
@@ -0,0 +1,41 @@
+
+
+    for (var i = 1; i < 10; i++) {
+        console.log(i);
+    }
+
+
+
+
+    var i = 1;
+    while(i<=10){
+        console.log(i);
+        i++; break;
+    }
+
+for (var i = 99; i >= 0; i--) {
+    console.log(i)
+}
+
+let word = "bottles";
+let count = 99;
+while (count > 0) {
+    if (count == 1){
+        word = "bottle"
+    }
+    console.log(count + " " + word + " of beer on the wall");
+    console.log(count + " " + word + " of beer,");
+    console.log("Take one down, pass it around,");
+    count = count - 1;
+    if (count > 0) {
+        if (count == 1){
+          console.log(count + " " + word.slice(0, -1) + " of beer on the wall.");
+        }
+      } else {
+        console.log("No more " + word + " of beer on the wall.");
+      }
+
+      const bools = [true, true, false, true, false, true, true, true]
+      for (var i = 0; i = true; i){
+          console.log(i)
+      }
